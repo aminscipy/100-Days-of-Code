cede2cb681c5ef935c3f84dbc3e2c974ae4db5cc
amin <thisisaamin@gmail.com>
2022-07-31 12:18:51 +0530

    Day 3 - Treasure island

---
+++ Day 3 - Treasure island.py
@@ -0,0 +1,18 @@
+
+# simple game base on conditional statements
+
+print('welcome to treasure island')
+door = input("to choose one door, type 'left' or 'right' : ").lower()
+
+if door == 'left':
+    swim = input("to swim type 'Y', or else type 'n' : ").lower()
+    if swim == 'n':
+        color_door = input("to choose color door type color of the door, 'blue', 'yellow', red' : ").lower()
+        if color_door == 'yellow':
+            print('you won')
+        else:
+            print('Game Over')
+    else:
+        print('Game Over')
+if door == 'right':
+    print('game over')
